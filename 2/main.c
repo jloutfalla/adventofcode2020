e13821d95699482a8fcce435741f8c73010166a7
Jean-Baptiste Loutfalla <jb.loutfalla@orange.fr>
2020-12-05 20:48:56 +0100

    Add COPYING

--- 2/main.c
+++ 2/main.c
@@ -1,3 +1,19 @@
+/* AdventOfCode 2020, Day 2 -- verify passwords validity
+   Copyright (C) 2020 Jean-Baptiste Loutfalla
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -81,9 +97,8 @@ int number_correct2(FILE *file)
       fscanf(file, "%d-%d %c: ", &min, &max, &c);
       fgets(str, MAX_LINE, file);
 
-      if (str[min - 1] != str[max - 1] &&
-          (str[min - 1] == c || str[max - 1] == c))
-          num++;
+      if (str[min - 1] != str[max - 1] && (str[min - 1] == c || str[max - 1] == c))
+        num++;
 
       min = -1;
       max = -1;
